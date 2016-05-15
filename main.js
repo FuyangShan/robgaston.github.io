ee39a57a9430efb23d589401965f0703489f5d64
Rob Gaston <rgaston@fargeo.com>
2016-05-15 09:52:00 -0700

    a little cleanup

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ require.config({
     paths: {
         'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min',
         'bootstrap': 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery']
+        }
     }
 });
-require(['jquery'], function ($) {
-    window.jQuery = $;
-    require(['bootstrap'], function () {
-        return;
-    });
-});
+require(['bootstrap']);
